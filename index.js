86f5d13d62b5e378b74ce3dd3ce68b5a641de86f
Migle <migluuk@gmail.com>
2025-04-01 11:26:20 +0300

    commit initial

---
+++ index.js
@@ -0,0 +1,17 @@
+// Failo isaugojimas CTRL +S
+
+// Single line comment
+
+/* Multi line comment */
+
+console.log(123456789);
+console.log(987); // gali buti ir salia eilutes
+console.log(3);
+console.log(2*2+2);
+
+console.log(1,2,3);
+console.log('Labas rytas, Lietuva');
+console.log('Protinga matematika:','2*2+2=',2*2+2);
+
+
+// komentaras gali buti bet kur, kompiuteris ji ignoruos
