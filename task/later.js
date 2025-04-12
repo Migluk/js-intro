96c6f93649f852ce8118485bff08151d63bd62c4
Migle <migluk720@gmail.com>
2025-04-12 16:36:38 +0300

    New

--- task/later.js
+++ task/later.js
@@ -1,30 +1,28 @@
-// 1. Patikrinkite, ar skaičius teigiamas. 
+/*1. užd. Padarykite funkciją, kuriai perdavus vieną iš (akmuo, popierius, šulinys) reikšmių, 
+kompiuteris su jumi sužaistų vaskiči raundą ir išvestų rezultatą į console. Kompiuterio 
+pasirinkimai turi būti atsitiktiniai.
+*/
 
-function arTeig(num) {
-    if (num > 0) {
-        return true;
-    } else {
-        return false;
+const words = ['akmuo', 'popierius', 'sulinys'];
+const number = Math.floor(Math.random() * words.length);
+const word = words[number];
+console.log(word);
+
+
+/*2 užd  Parašykite funkcija, kuri parodytų kiek žmonių reikia norint pakelti tam tikro svorio 
+krovinį, kai vienas žmogus gali pakelti x kg. Pvz: 10kg svorį turi kelti 5 žmonės, jei vienas jų gali 
+pakelti 2.4 kg 
+*/
+
+function howMuch(weight, peopleWeight) {
+    if (peopleWeight <= 0) {
+        return 'Negali pakelti krovinio';
     }
-}
-console.log(arTeig(3));
-
-console.log('-----------');
-
-// 2. Patikrinkite, ar skaičius dalijasi iš 5. 
-
-function arDalijas(num) {
-    return num % 5 === 0;
+    const neededPeople = Math.ceil(weight / peopleWeight);
+    return neededPeople;
 }
 
-console.log(arDalijas(15));
+const package = 50;
+const onePerson = 5.7;
 
-// 3. Patikrinkite, ar teksto tipo kintamajame yra daugiau nei 5 simboliai.
-
-function nameLenght(name){
-    const size = name.length;
-    return `Zodzis "${name}" turi ${size} raides.`;
-    
-}
-const num1 = nameLenght('Drugelis');
-console.log(num1);
+console.log(`Reikia ${howMuch(package, onePerson)} zmoniu.`);
